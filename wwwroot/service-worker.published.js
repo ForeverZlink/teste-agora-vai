559224a5cbc2c9ed01e3589c96ffcd84eae03be5
Carlos Henrique Cunha de Oliveira <74561046+ForeverZlink@users.noreply.github.com>
2025-03-03 21:29:25 -0300

    Update service-worker.published.js ATUALIZAÇÃO IPHONE
    ATUALIZAÇÃO IPHONE

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -45,18 +45,18 @@ async function onActivate(event) {
 }
 
 async function onFetch(event) {
-    let cachedResponse = null;
-    if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache,
-        // unless that request is for an offline resource.
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate'
-            && !manifestUrlList.some(url => url === event.request.url);
+    if (event.request.method !== 'GET') return fetch(event.request);
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
+    const cache = await caches.open(cacheName);
+    let response = await cache.match(event.request);
+
+    if (!response) {
+        try {
+            response = await fetch(event.request);
+        } catch (error) {
+            console.warn("Network request failed, serving index.html");
+            response = await cache.match('index.html'); // Fallback para manter a navegação funcionando
+        }
     }
-
-    return cachedResponse || fetch(event.request);
+    return response;
 }
